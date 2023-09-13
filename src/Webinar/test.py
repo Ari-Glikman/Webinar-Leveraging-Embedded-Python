ed8b5ab8facb428656ba429ccecc63cf535057f0
Ari-Glikman <73805987+Ari-Glikman@users.noreply.github.com>
2023-09-13 12:03:34 +0300

    Create test.py

---
+++ src/Webinar/test.py
@@ -0,0 +1,12 @@
+# print the members of the Fibonacci series that are less than 10
+print('Fibonacci series:')
+a, b = 0, 1
+while a < 10:
+    print(a, end=' ')
+    a, b = b, a + b
+
+# import the iris module and run instance method
+import iris
+print('\nRun IRIS Code:\n')
+person = iris.cls('Webinar.Human')._OpenId(1)
+person.PythonPrint()
